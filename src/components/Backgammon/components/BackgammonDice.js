889772ab19c1f2ad12eb29669f701f0856b246fd
Michael Perkins <mperkins1995@gmail.com>
2023-07-21 23:18:12 -0400

    style dice

--- src/components/Backgammon/components/BackgammonDice.js
+++ src/components/Backgammon/components/BackgammonDice.js
@@ -3,13 +3,24 @@ import React from "react";
 const BackgammonDice = ({ diceValues }) => {
   // Add the dice rendering logic here
   return (
-    <div>
-      <h3>Dice Values:</h3>
-      {diceValues.length > 0 ? (
-        <p>{diceValues.join(", ")}</p>
-      ) : (
-        <button>Roll Dice</button>
-      )}
+    <div className="text-center mt-4">
+      <h3 className="text-lg font-semibold mb-2">Dice Values:</h3>
+      <div className="flex items-center justify-center">
+        {diceValues.length > 0 ? (
+          diceValues.map((value, index) => (
+            <div
+              key={index}
+              className="dice-border bg-white w-16 h-16 rounded-md border border-gray-400 flex items-center justify-center mx-1"
+            >
+              <span className="text-3xl font-bold">{value}</span>
+            </div>
+          ))
+        ) : (
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600">
+            Roll Dice
+          </button>
+        )}
+      </div>
     </div>
   );
 };
